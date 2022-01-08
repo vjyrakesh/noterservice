b56ba5ce7c04fc383fb5ea2937b6035e5ea318c2
Rakesh Kasibhatla <vjyrakesh@gmail.com>
2022-01-08 12:44:31 +0530

    First commit

---
+++ src/main/java/com/rkasibhatla/noterservice/NoterserviceApplication.java
@@ -0,0 +1,13 @@
+package com.rkasibhatla.noterservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoterserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoterserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rkasibhatla/noterservice/NoterserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rkasibhatla.noterservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoterserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
