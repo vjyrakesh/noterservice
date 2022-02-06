8f1515bf7ce38a79cc71670f2413f5ea39a60d94
Rakesh Kasibhatla <vjyrakesh@gmail.com>
2022-02-06 18:19:14 +0530

    Fix login issue

--- src/main/java/com/rkasibhatla/noterservice/configuration/NoterSeurityConfig.java
+++ src/main/java/com/rkasibhatla/noterservice/configuration/NoterSeurityConfig.java
@@ -29,9 +29,9 @@ public class NoterSeurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
-        corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
+        corsConfiguration.setAllowedOriginPatterns(Arrays.asList("*"));
         corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PUT","OPTIONS","PATCH", "DELETE"));
-//        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setAllowCredentials(true);
         corsConfiguration.setExposedHeaders(Arrays.asList("Authorization"));
 
         http
